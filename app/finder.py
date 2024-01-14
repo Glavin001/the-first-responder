9b90833056a50cc94069e597008bed0c24b9cdab
mkovetsky <mkovetsky@gmail.com>
2024-01-13 19:10:39 -0800

    mike: build docs in finder

--- app/finder.py
+++ app/finder.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from git import Repo
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 
 
 def find_vulnerabilities(code):
@@ -8,25 +9,40 @@ def find_vulnerabilities(code):
     return True
 
 
+def get_report(vul) -> str:
+    return ""
+
+
 def get_latest_commit_author(file_path):
     result = subprocess.run(['git', 'blame', file_path, '-l', '-L', '1,1'], stdout=subprocess.PIPE)
     return result.stdout.decode().split()[1]
 
 
-def process_repo(url):
-    repo_path = '/Users/m/Repositories/the-first-responder/vulnerable_code'
+def _split(file_path: str, code: str):
+    author = get_latest_commit_author(file_path)
+
+    python_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON, chunk_size=200, chunk_overlap=0
+    )
+    documents = python_splitter.create_documents([code], metadatas=[{"author": author, "file_path": file_path}])
+    print(documents)
+
+
+def process_repo():
+    reports = []
+    repo_path = '/Users/m/Repositories/responder/the-first-responder/vulnerable_code'
     for root, dirs, files in os.walk(repo_path):
         for file in files:
             file_path = os.path.join(root, file)
             with open(file_path, 'r') as f:
                 code = f.read()
 
-            vulnerabilities = find_vulnerabilities(code)
-            if vulnerabilities:
-                author = get_latest_commit_author(file_path)
-                print(f"Vulnerabilities in {file_path} by {author}")
+            vulnerability = find_vulnerabilities(code)
+            if vulnerability:
+                _split(file_path, code)
+                reports.append(get_report(vulnerability))
 
 
 # Example usage
 if __name__ == "__main__":
-    process_repo('https://github.com/MikeKovetsky/vulnarable-code-demo')
+    process_repo()
--- vulnerable_code/upload_image.py
+++ vulnerable_code/upload_image.py
@@ -1,22 +1,16 @@
-import os
-
 def upload_to_gitlab(image_file, gitlab_url, repository):
-    """
-    Uploads an image to a GitLab repository after validation.
-    Args:
-    image_file (str): The path to the image file.
-    gitlab_url (str): The URL of the GitLab instance.
-    repository (str): The target repository in GitLab.
-    """
     if validate_image(image_file):
         _upload(image_file)
     else:
         raise Exception("Invalid image file")
 
+
 def validate_image(image_file):
+    """Image_file is a FILE"""
     """TODO: Implement the actual image validation logic"""
-    print(f"Validating {image_file}...")
     return True
 
-def _upload(file): 
+
+def _upload(file):
+    """TODO: Implement the upload"""
     pass
